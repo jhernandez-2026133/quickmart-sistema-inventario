8e01a002d4fbb3c75e7cfc1d5bc017d1e7307f11
Miguel Pérez <mperez-2026233@kinal.edu.gt>
2026-09-08 17:10:14 -0600

    Se crearon las clases en el paquete modelo y la validacion del login en DB

---
+++ src/org/quickmartkinal/system/model/Rol.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.quickmartkinal.system.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class Rol {
+    
+    private String idRol;
+    private String nombreRol;
+    private String descripcionRol;
+    
+    public Rol () {
+        
+    }
+
+    public Rol(String idRol, String nombreRol, String descripcionRol) {
+        this.idRol = idRol;
+        this.nombreRol = nombreRol;
+        this.descripcionRol = descripcionRol;
+    }
+
+    public String getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(String idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    public String getDescripcionRol() {
+        return descripcionRol;
+    }
+
+    public void setDescripcionRol(String descripcionRol) {
+        this.descripcionRol = descripcionRol;
+    }
+    
+}
