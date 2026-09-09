07578e4a50772d4e6b9bc385ce15813efa8707bd
Jose Hernandez <jhernandez-2026133@kinal.edu.gt>
2026-09-09 16:40:50 -0600

    feat: Se realizo la validacion login, register y base de datos

--- src/org/quickmartkinal/system/utils/ViewFactory.java
+++ src/org/quickmartkinal/system/utils/ViewFactory.java
@@ -1,92 +1,9 @@
-<<<<<<< HEAD
-=======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
->>>>>>> develop
 package org.quickmartkinal.system.utils;
 
-<<<<<<< HEAD
-
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import java.net.URL;
-import javafx.fxml.JavaFXBuilderFactory;
-import org.quickmartkinal.system.ClasePrincipal;
-
-
-public class ViewFactory {
-
-    private final String PATH_VIEWS="/org/quickmartkinal/system/view/";
-
-    public Scene loadFileFXML(String nameFile, int width, int height){
-        String pathOfFile = PATH_VIEWS + nameFile;
-
-       try{
-           FXMLLoader loadFXML = new FXMLLoader();
-           URL urlFile =  ClasePrincipal.class.getResource(pathOfFile);
-           loadFXML.setBuilderFactory( new JavaFXBuilderFactory() );
-           loadFXML.setLocation(urlFile);
-
-           return new Scene( loadFXML.load(), width, height);
-
-       }catch (IOException e){
-           throw new UncheckedIOException(e);
-       }
-    }
-
-    public void loadScene( String nameFile){
-        Scene scene = null;
-        try {
-            switch ( nameFile ) {
-                case "login" -> {
-                    SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setTitle("Iniciar Sesion");
-                    SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setResizable(false);
-                    scene = loadFileFXML("loginView.fxml",400,500);
-                }
-                case "register" -> {
-                       SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setTitle("Registro de Usuario");
-                    SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setResizable(false);
-                    scene = loadFileFXML("RegisterView.fxml", 350, 400);
-                }
-                case "dashboard" -> {
-                    SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setTitle("Dashboard");
-                    SceneManager.getInstanciaSceneManager()
-                            .getStagePrincipal().setResizable(true);
-                    scene = loadFileFXML("DashboardView.fxml", 800, 520);
-                }
-                default -> scene = loadFileFXML("",0,0);
-            }
-
-            SceneManager.getInstanciaSceneManager().chanceScene(scene);
-
-        } catch (NullPointerException e) {
-            System.out.println("Error load scene");
-        }
-    }
-
-
-    public void viewRegister(){
-        loadScene("register");
-    }
-
-    public void viewLogin(){
-        loadScene("login");
-    }
-
-    public void viewDashboard(){
-        loadScene("dashboard");
-    }
-
-=======
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URL;
@@ -131,6 +48,11 @@ public class ViewFactory {
                     SceneManager.getInstanciaSceneManager().getStagePrincipal().setResizable(false);
                     scene = loadFileFXML("loginViewMarket.fxml", 400, 500);
                 }
+                case "register" -> {
+                    SceneManager.getInstanciaSceneManager().getStagePrincipal().setTitle("QUICKMART - CREAR CUENTA");
+                    SceneManager.getInstanciaSceneManager().getStagePrincipal().setResizable(false);
+                    scene = loadFileFXML("loginRegisterMarket.fxml", 350, 500);
+                }
                 default -> scene = loadFileFXML("loginViewMarket.fxml", 400, 500);
             }
 
@@ -143,6 +65,10 @@ public class ViewFactory {
 
     public void viewLogin() {
         loadScene("login");
+    }
+
+    public void viewRegister() {
+        loadScene("register");
     }
 
     public void viewDashboard(Usuario usuario) {
@@ -170,9 +96,5 @@ public class ViewFactory {
             alertInfo.viewAlert("ERROR", "ERROR AL CARGAR", "ERROR AL ABRIR EL MODULO", "Ocurrio un error, intenta nuevamente.");
         }
     }
-<<<<<<< HEAD
->>>>>>> 05be444258df5c84736a3d2340d725e86c358462
-=======
 
->>>>>>> develop
 }
