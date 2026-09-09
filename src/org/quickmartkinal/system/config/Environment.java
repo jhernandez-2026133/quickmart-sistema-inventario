dd482d2f8d4b44872dce0807156961256ad30c3d
Informatica <dmarroquin-2026289@kinal.edu.gt>
2026-09-09 16:36:50 -0600

    ft: se agreso y actualizo la rama

---
+++ src/org/quickmartkinal/system/config/Environment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.quickmartkinal.system.config;
+
+/**
+ *
+ * @author informatica
+ */
+public class Environment {
+    protected static final String USER = "IN4AV";
+    protected static final String PASSWORD ="&mnid4AV";
+    protected static final String DATA_BASE="proyecto-quickmart-inventory-kinal-in4av";
+    protected static final String LOCATION_SERVICE="localhost:3306";
+}
